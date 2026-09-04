10bf9f6a1421d6c1fae8d895d1ddeb03f4ce2532
Shubham <s.v.dongare@accenture.com>
2026-09-04 15:00:30 +0530

    Initial commit

---
+++ src/test/java/com/ai_support_ticket_triage/ai/AiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.ai_support_ticket_triage.ai;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
